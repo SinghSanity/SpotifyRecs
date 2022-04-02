46f540a8ff9390174e255794f51b0f6554422b5c
Amandeep <asinghsanity@gmail.com>
2022-04-02 19:37:59 -0400

    Added a new file with searching function

---
+++ backend/search.py
@@ -0,0 +1,22 @@
+from re import A
+import requests
+from auth import get_auth
+
+#This method will search for an artist and get their URI to send to data.py
+def search(artist):
+    access_token = get_auth()
+    headers = {
+        'Authorization': 'Bearer {TOKEN}'.format(TOKEN=access_token)
+    }
+    URL = 'https://api.spotify.com/v1/search'
+    data = requests.get(URL + "?q=" + artist + "&type=artist", headers = headers)
+    data = data.json()
+    
+    artist_list = data['artists']['items']
+
+    if artist_list == []:
+        return None
+    else:
+        artist = artist_list[0]['uri']
+        artist = artist.split('spotify:artist:')[1]
+        return artist
